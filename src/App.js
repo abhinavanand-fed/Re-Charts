dc6e0e18c7a38501e12c8bac12893294610e51c1
abhinavanand-fed <abhinavanand080@gmail.com>
2023-02-11 13:39:38 +0530

    recharts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import './LineChartExample.css';
 
-function App() {
+const LineChartExample = () => {
+  const [data, setData] = useState([
+    { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+    { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+    { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+    { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+    { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+    { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+    { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+  ]);
+
+  const [newData, setNewData] = useState({ name: '', uv: 0, pv: 0, amt: 0 });
+
+  const handleChange = (e) => {
+    setNewData({ ...newData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setData([...data, newData]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="line-chart-container">
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={newData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="UV"
+          name="uv"
+          value={newData.uv}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="PV"
+          name="pv"
+          value={newData.pv}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="AMT"
+          name="amt"
+          value={newData.amt}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
-}
+};
 
-export default App;
+export default LineChartExample
